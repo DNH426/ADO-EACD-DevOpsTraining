be1176478e51afcfc7abccd62f15359cf86b4eb7
dhr2505 <daniel.hagy@northwestern.edu>
2020-09-28 16:40:10 -0500

    added lambda bucket

--- dnh/variables.tf
+++ dnh/variables.tf
@@ -7,8 +7,9 @@ module "build_wildRydes" {
   app_name = "wildRydes"
   env      = "dnh"
 
-  #S3 Bucket name
-  bucket   = "wild-rydes-bucket"
+  #S3 Bucket names
+  bucket        = "wild-rydes-bucket"
+  lambda_bucket = "wild-rydes-lambda-bucket"
 
   # Lambda Variables
   # lambda_runtimes  = ["nodejs10.x"]
