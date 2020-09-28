ea45da1a342cc3b7af7e64c55a7accb7424f7fe5
dhr2505 <daniel.hagy@northwestern.edu>
2020-09-28 15:19:28 -0500

    fix

--- dnh/variables.tf
+++ dnh/variables.tf
@@ -1,9 +1,22 @@
 module "build_wildRydes" {
   # globals
   source   = "../modules"
-  app_name = var.app_name
-  env      = var.env
-  region   = var.region
+  
+  # Global Variables
+  region   = "us-east-2"
+  app_name = "wildRydes"
+  env      = "dnh"
 
+  #S3 Bucket name
+  bucket   = "wild-rydes-bucket"
+
+  # Lambda Variables
+  lambda_runtimes  = ["nodejs10.x"]
+
+  # API GW
+
+
+  # account_resources_state_bucket = "as-dma-sbx-tfstate"
+  # account_resources_state_file = "dma-shared-resources/sbx/terraform.tfstate"
 }
 
